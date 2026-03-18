e4eb52e011818c5c2a383dbe4c855d4dd6164e29
yuki <uk.tmbc2408@gmail.com>
2026-03-18 21:30:48 +0900

    ok

--- プライマー作成.jsx
+++ プライマー作成.jsx
@@ -1,11 +1,11 @@
 var doc = app.activeDocument;
 var selection = app.selection;
 
-    try {
-        var sw_primer = doc.swatches.getByName("RDG_PRIMER");
-    } catch (e) {
-        alert("RDG_PRIMERまたはRDG_WHITEスウォッチがドキュメント内に見つかりません。");
-    }
+try {
+    var sw_primer = doc.swatches.getByName("RDG_PRIMER");
+} catch (e) {
+    alert("RDG_PRIMERまたはRDG_WHITEスウォッチがドキュメント内に見つかりません。");
+}
 
 if (selection.length!==0){
     for(var i=0;i<selection.length;i++){
@@ -13,7 +13,6 @@ if (selection.length!==0){
             var src = selection[i];
             primer_tf = selection[i].duplicate(src, ElementPlacement.PLACEAFTER);
             primer_tf.textRange.characterAttributes.fillColor = sw_primer.color;
-
         }
     }
 } else {
