4202242728f60c9792319e8b1ca262fafc9a0f23
yuki <uk.tmbc2408@gmail.com>
2026-03-18 21:29:53 +0900

    ok

---
+++ プライマー作成.jsx
@@ -0,0 +1,21 @@
+var doc = app.activeDocument;
+var selection = app.selection;
+
+    try {
+        var sw_primer = doc.swatches.getByName("RDG_PRIMER");
+    } catch (e) {
+        alert("RDG_PRIMERまたはRDG_WHITEスウォッチがドキュメント内に見つかりません。");
+    }
+
+if (selection.length!==0){
+    for(var i=0;i<selection.length;i++){
+        if (selection[i].typename==="TextFrame") {
+            var src = selection[i];
+            primer_tf = selection[i].duplicate(src, ElementPlacement.PLACEAFTER);
+            primer_tf.textRange.characterAttributes.fillColor = sw_primer.color;
+
+        }
+    }
+} else {
+    alert("テキストを選択してください");
+}
